cb8a8d81806706da493fac637d93ff9a8ef77957
IkimonoJanai <rnbominami159357@gmail.com>
2018-10-05 21:42:26 +0900

    DCM new Branch nay

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,27 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picSaberUwU = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picSaberUwU)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(232, 242);
+            this.button1.Location = new System.Drawing.Point(174, 197);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(249, 97);
+            this.button1.Size = new System.Drawing.Size(187, 79);
             this.button1.TabIndex = 0;
             this.button1.Text = "Nhấp vào để tìm hiểu về Danh";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(2, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 83);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh\'s Fact";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picSaberUwU
+            // 
+            this.picSaberUwU.Image = global::DemoOsuDaTrashGemu.Properties.Resources._12188955_498880390293647_3589557645439156003_n;
+            this.picSaberUwU.Location = new System.Drawing.Point(-2, 318);
+            this.picSaberUwU.Name = "picSaberUwU";
+            this.picSaberUwU.Size = new System.Drawing.Size(96, 110);
+            this.picSaberUwU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSaberUwU.TabIndex = 2;
+            this.picSaberUwU.TabStop = false;
+            this.picSaberUwU.Click += new System.EventHandler(this.picSaberUwU_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 524);
+            this.ClientSize = new System.Drawing.Size(518, 426);
+            this.Controls.Add(this.picSaberUwU);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.picSaberUwU)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +94,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picSaberUwU;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -11,14 +11,32 @@ namespace DemoOsuDaTrashGemu
 {
     public partial class Form2 : Form
     {
+        int x = -100;
+        int y =-10;
         public Form2()
         {
+            
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Danh Ngu vl ra");
+        }
+
+        private void picSaberUwU_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            picSaberUwU.Left += x;
+            picSaberUwU.Top += y;
+            if (picSaberUwU.Left <= 0 || picSaberUwU.Right >= ClientRectangle.Width)
+                x = -x;
+            if (picSaberUwU.Top < -150 || picSaberUwU.Bottom >= ClientRectangle.Height + 150)
+                y = -y;
         }
     }
 }
