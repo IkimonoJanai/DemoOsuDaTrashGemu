e3e9c6cacfa3bc880c29850741d673944edcbdc2
IkimonoJanai <rnbominami159357@gmail.com>
2018-10-05 22:20:02 +0900

    DCM new branch nay

--- Form2.cs
+++ Form2.cs
@@ -11,6 +11,9 @@ namespace DemoOsuDaTrashGemu
 {
     public partial class Form2 : Form
     {
+        int x = -100, y = -20;
+        int xArrow = 0, yArrow = 25 , xArrowBot = -5 , xArrowRight = 5;
+        
         public Form2()
         {
             InitializeComponent();
@@ -19,6 +22,25 @@ namespace DemoOsuDaTrashGemu
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Danh Ngu vl ra");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            picSaberUwU.Left += x;
+            picSaberUwU.Top += y;
+            if (picSaberUwU.Left <= 0 || picSaberUwU.Right >= ClientRectangle.Width)
+                x = -x;
+            if (picSaberUwU.Top <= -150 || picSaberUwU.Bottom >= ClientRectangle.Height + 150)
+                y = -y;
+            picArrow.Left += xArrow;
+            picArrow.Top += yArrow;
+            if (picArrow.Left <= 0 || picArrow.Right >= ClientRectangle.Width)
+                xArrow = -xArrow;
+            if (picArrow.Top <= -20 || picArrow.Bottom >= ClientRectangle.Height)
+                yArrow = -yArrow;
+            //picArrow.Right += xArrowRight; // read only cant write
+            //picArrow.Bottom += xArrowBot; // read only cant write
+
         }
     }
 }
